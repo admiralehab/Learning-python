d025a00af21074761e42ac3a4911f6efa90c17e5
Admiralehab <ehab238@protonmail.com>
2025-05-25 21:34:58 +0200

    and some more

---
+++ Numbers.py
@@ -0,0 +1,23 @@
+my_num = -5
+from math import *
+
+print (2)
+print (2.0987)
+print (-2.0987)
+print (3+5)
+print(5-2)
+print (3*4)
+print(3*(4 + 5))
+print (10 % 3)
+print (my_num)
+print(str(my_num) + (" my favorite number"))
+print(my_num)
+print(abs(my_num))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
