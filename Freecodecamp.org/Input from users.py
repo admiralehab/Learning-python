9e9a2048aec72cb766cbe3cffd60cb1bce9aa2de
admiralehab <ehab238@protonmail.com>
2025-05-27 16:36:55 +0200

    made some changes

--- Freecodecamp.org/Input from users.py
+++ Freecodecamp.org/Input from users.py
@@ -1,11 +1,11 @@
 
 name = input("Enter your name: ")
 age = input ("Enter your age: ")
-street = input ("Enter your street: ")
-zipcode = input ("Enter your zipcode: ")
+street = input ("Enter your street and house number: ")
+zipcode = input ("Enter your zipcode and city: ")
 Country = input ("Enter your counrty: ")
 
 print("Hello," + name + "!" )
 print ("Your age is " + age + "!")
-print ("And your adress is " + street + " " + zipcode +" "+ Country)
+print ("And your adress is "+ "\n" + street + "\n " + zipcode +" \n"+ Country)
 print ("Thanks for talking to me :) ")
